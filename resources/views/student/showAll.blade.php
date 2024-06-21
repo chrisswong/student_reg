99703ac676fdacfe5d077d3ee513735dc1ea8ae7
Chris Wong <chriswong.broadband@gmail.com>
2024-06-21 18:29:33 +0100

    show course enrolment for each student

--- resources/views/student/showAll.blade.php
+++ resources/views/student/showAll.blade.php
@@ -3,6 +3,9 @@
 
     @forelse ($students as $student)
         <li>{{ $student->name }}</li>
+        @foreach($student->courses as $course)
+            <span> {{ $course->name }} </span>
+        @endforeach
     @empty
         <p>No students</p>
     @endforelse
