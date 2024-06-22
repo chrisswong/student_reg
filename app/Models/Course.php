fdab1b9ad5fa4aed2667227653aab0ec0c7193c8
Chris Wong <chriswong.broadband@gmail.com>
2024-06-22 19:14:43 +0100

    add soft delete to course and student

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function students() {
         return $this->belongsToMany(Student::class, 'student_enrolment', 'course_id', 'student_id');
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function courses() {
         return $this->belongsToMany(Course::class, 'student_enrolment', 'student_id', 'course_id');
